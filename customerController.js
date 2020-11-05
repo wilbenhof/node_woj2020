7072cdba91f27643e1e19d6c60cf7a3bd494857f
wilbenhof <ville_gronvall@hotmail.com>
2020-11-05 14:03:44 +0200

    Tehtävä 2, osa1

--- customerController.js
+++ customerController.js
@@ -7,7 +7,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host : 'localhost', // tietokantapalvelimen osoite
-  port : 3307, // oletuksen 3006
+  port : 3307, // oletuksen 3306
   user : 'root', // kehitystarkoituksessa voidaan käyttää root-käyttäjää. Tuotannossa ei saa käyttää root-käyttäjää
   password : 'root', // voi olla tyhjäkin, käyttäkää sitä mikä teil o
   database : 'asiakas' // 
@@ -33,6 +33,19 @@ module.exports =
     },
 
     fetchAll: function(req, res){
+      // Tehtävä 2 node.js tehtävistä
+      connection.query('SELECT *, FROM Asiakas', function(error, results, fields){
+        if ( error ) {
+          console.log("Virhe, dataa ei saatu: " + error);
+        }
+        else
+        {
+          console.log("Data = " + JSON.stringify(results));
+          res.json(results);
+        }
+      })
+      // Tehtävä 2 node.js tehtävistä
+
       console.log("Body = " +JSON.stringify(req.body));
       console.log("Params = " +JSON.stringify(req.query));
       console.log(req.query.nimi);
